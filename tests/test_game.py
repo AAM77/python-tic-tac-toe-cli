3d2e85b9506bcfc53b565b36cc4a6f978ce149b5
Mohammad Adeel <adeel.a.mhd@gmail.com>
2021-08-16 20:29:14 -0400

    Add tests to check if get_desired_position prompts user to enter a valid input on invalid entry.

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,3 +1,4 @@
+from io import StringIO
 import pytest
 from src.game import Game
 
@@ -47,11 +48,67 @@ def test_determine_winner_is_o():
     game.board = ['O', 'O', 'O', 'X', 'O', 'X', 'X', 'X', '9']
     assert game.determine_winner() == 'O'
 
-def test_get_desired_position(capfd):
+def test_get_desired_position(capfd, monkeypatch):
+    user_input = StringIO('1\n')
+    monkeypatch.setattr('sys.stdin', user_input)
+
     game = Game()
     game.get_desired_position()
     out, err = capfd.readouterr()
     assert out == "Please select a position to place your mark.\n"
+
+def test_get_desired_position_is_a_valid_number(monkeypatch):
+    user_input = StringIO('1\n')
+    monkeypatch.setattr('sys.stdin', user_input)
+
+    game = Game()
+    game.get_desired_position()
+    assert game.player_selection.isdigit() == True
+    assert game.player_selection == '1'
+
+def test_get_desired_position_alerts_user_on_invalid_input_negative(capfd, monkeypatch):
+    user_input = StringIO('-2\n')
+    monkeypatch.setattr('sys.stdin', user_input)
+
+    game = Game()
+    game.get_desired_position()
+
+    out, _ = capfd.readouterr()
+    assert "\nInvalid selection. Enter a number from 1 to 9.\n" in out
+    assert game.player_selection == ''
+
+def test_get_desired_position_alerts_user_on_invalid_input_char(capfd, monkeypatch):
+    user_input = StringIO('a\n')
+    monkeypatch.setattr('sys.stdin', user_input)
+
+    game = Game()
+    game.get_desired_position()
+
+    out, _ = capfd.readouterr()
+    assert "\nInvalid selection. Enter a number from 1 to 9.\n" in out
+    assert game.player_selection == ''
+
+def test_get_desired_position_alerts_user_on_invalid_input_zero(capfd, monkeypatch):
+    user_input = StringIO('0\n')
+    monkeypatch.setattr('sys.stdin', user_input)
+
+    game = Game()
+    game.get_desired_position()
+
+    out, _ = capfd.readouterr()
+    assert "\nInvalid selection. Enter a number from 1 to 9.\n" in out
+    assert game.player_selection == ''
+
+def test_get_desired_position_alerts_user_on_invalid_input_ten(capfd, monkeypatch):
+    user_input = StringIO('10\n')
+    monkeypatch.setattr('sys.stdin', user_input)
+
+    game = Game()
+    game.get_desired_position()
+
+    out, _ = capfd.readouterr()
+    assert "\nInvalid selection. Enter a number from 1 to 9.\n" in out
+    assert game.player_selection == ''
 
 def test_update_board():
     game = Game()
