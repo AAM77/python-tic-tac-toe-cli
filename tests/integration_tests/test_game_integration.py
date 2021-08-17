c1f36ec57fab63ea70138e6eed55192524697f0e
Mohammad Adeel <adeel.a.mhd@gmail.com>
2021-08-17 12:43:20 -0400

    Add single integration test that initiates a player turn and checks to ensure that the correct steps are taken and the board is updated correctly.

--- tests/integration_tests/test_game_integration.py
+++ tests/integration_tests/test_game_integration.py
@@ -1,12 +1,13 @@
 from io import StringIO
 from src.game import Game
 
-def test_initiate_turn():
+def test_take_turn(capfd, monkeypatch):
+    user_input = StringIO('2\n')
+    monkeypatch.setattr('sys.stdin', user_input)
+
     game = Game()
-    assert game.current_player() == 'X'
-    # check if the game is over (someone won or it is a tie)
-    # check which user's turn it is
-    # ask the user for input
-    # update the board with the user's input
-    # display the board
-    pass
+    game.take_turn()
+    out, _ = capfd.readouterr()
+    assert game.player_selection == "2"
+    assert game.board == ["1", "X", "3", "4", "5", "6", "7", "8", "9"]
+    assert " 1 | X | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n" in out
