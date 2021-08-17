e02ec3fa402bf17a218f5dd98083123f91889e91
Mohammad Adeel <adeel.a.mhd@gmail.com>
2021-08-17 12:17:21 -0400

    Remove type hinting from self because it is unnecessary and only adds complexity.

--- src/game.py
+++ src/game.py
@@ -1,5 +1,6 @@
+from typing import Type
 class Game:
-    def __init__(self: object):
+    def __init__(self):
         self.board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         self.player_selection = ""
 
@@ -15,32 +16,32 @@ class Game:
         ]
 
 
-    def display_board(self: object):
+    def display_board(self):
         print(f" {' | '.join(self.board[0:3])} ")
         print("-----------")
         print(f" {' | '.join(self.board[3:6])} ")
         print("-----------")
         print(f" {' | '.join(self.board[6:10])} ")
 
-    def current_player(self: object) -> str:
+    def current_player(self) -> str:
         if self.board.count('X') >= 1 and self.board.count('X') % 2 == 0:
             return 'O'
         else:
             return 'X'
 
-    def get_desired_position(self: object):
+    def get_desired_position(self):
         self.player_selection = input("Please select a position to place your mark.\n")
 
         if self.player_selection not in [str(i) for i in range(1, 10)]:
             self.reset_player_selection()
             print("Invalid selection. Enter a number from 1 to 9.")
 
-    def update_board(self: object):
+    def update_board(self):
         new_board = self.board.copy()
         new_board[int(self.player_selection) - 1] = self.current_player()
         self.board = new_board
 
-    def is_winner(self: object) -> bool:
+    def is_winner(self) -> bool:
         for combination in self.WINNING_COMBINATIONS:
             if (
                 all(self.board[index] == 'X' for index in combination) or
@@ -49,25 +50,25 @@ class Game:
                 return True
         return False
 
-    def is_tie(self: object) -> bool:
+    def is_tie(self) -> bool:
         if not self.is_winner() and all(item in ['X', 'O'] for item in self.board):
             return True
         return False
 
-    def determine_winner(self: object) -> str:
+    def determine_winner(self) -> str:
         for combination in self.WINNING_COMBINATIONS:
             if all(self.board[index] == 'X' for index in combination):
                 return 'X'
             elif all(self.board[index] == 'O' for index in combination):
                 return 'O'
 
-    def reset_board(self: object):
+    def reset_board(self):
         self.board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-    def reset_player_selection(self: object):
+    def reset_player_selection(self):
         self.player_selection = ""
 
-    def initiate_turn(self: object):
+    def initiate_turn(self):
         pass
         # check if the game is over (someone won or it is a tie)
         # check which user's turn it is
